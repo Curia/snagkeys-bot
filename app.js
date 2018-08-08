be0be5ff70297cfc8b973e3ead8c916ed30ae6ce
Adam  Phillips <aphillips@squiz.net>
2018-08-08 18:05:11 +1000

    Added shutdown command

--- app.js
+++ app.js
@@ -11,21 +11,39 @@ const config = require("./config.json");
 
 // On bot ready.
 client.on('ready', () => {
-    const chanList = client.channels;
     const guildList = client.guilds;
 
     console.log(`Bot initiated! \n`);
-    console.log(`Channels avaliable: \n`);
-    chanList.map(c => {
-        console.log(`Id: ${c.id}`)
-        console.log(`Name: ${c.name} \n`)
-    });
+    listChannels();
 });
 
 // Create an event listener for messages
 client.on('message', message => {
     console.log(`message sent in ${message.channel}`)
+
+    switch(message.content.toLowerCase()) {
+        case `${config.prefix}destroy`:
+            closeBot(message.channel, message.author);
+            break;
+    }
 });
+
+const listChannels = () => {
+    const chanList = client.channels;
+
+    console.log(`Channels avaliable: \n`);
+    chanList.map(c => {
+        console.log(`Id: ${c.id}`)
+        console.log(`Name: ${c.name} \n`)
+    });
+}
+
+
+const closeBot = (channel, author) => {
+    channel.send('Beep boop, shutting down....')
+    .then(msg => client.destroy())
+    .then(console.log(`Bot shutdown @ ${new Date()} by ${author.username} : #${author.id}`))
+}
 
 // Log our bot in
 client.login(config.token);
