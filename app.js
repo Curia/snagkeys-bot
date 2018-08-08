511a6ab2b73cbfb8afe8b4425aad2cd2da49b04c
Adam  Phillips <aphillips@squiz.net>
2018-08-08 16:41:12 +1000

    Some basic reporting of channels added

--- app.js
+++ app.js
@@ -11,16 +11,20 @@ const config = require("./config.json");
 
 // On bot ready.
 client.on('ready', () => {
-    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+    const chanList = client.channels;
+    const guildList = client.guilds;
+
+    console.log(`Bot initiated! \n`);
+    console.log(`Channels avaliable: \n`);
+    chanList.map(c => {
+        console.log(`Id: ${c.id}`)
+        console.log(`Name: ${c.name} \n`)
+    });
 });
 
 // Create an event listener for messages
 client.on('message', message => {
-    // If the message is "ping"
-    if (message.content === 'ping') {
-        // Send "pong" to the same channel
-        message.channel.send('pong');
-    }
+    console.log(`message sent in ${message.channel}`)
 });
 
 // Log our bot in
