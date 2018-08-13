b86c935bf443656b3b1a8bdaed3c2f35aec14e5a
Adam  Phillips <aphillips@squiz.net>
2018-08-13 16:20:47 +1000

    Cleaned up app.js plus config change

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 const Discord = require("discord.js");
 const moment = require("moment");
 
-// This is your client.
 const client = new Discord.Client();
-
-// Load config
 const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
 
-// On bot ready.
 client.on('ready', () => {
-    const guildList = client.guilds;
-
     console.log(`Bot initiated! \n`);
-    listChannels();
 });
 
-// Create an event listener for messages
 client.on('message', message => {
-    switch(message.content) {
-        case `${config.prefix}destroy`:
-            closeBot(message);
+    
+    // Dirty way to only allow users in config.roles list to execute commands.
+    if (authUser()) return false;
+    logInput(message);
+
+    switch (message.content) {
+        // Force shutdown bot
+        case `${config.prefix}shutdown`:
+            shutdown(message);
             break;
+        // Removes old listings from market channels
         case `${config.prefix}prune`:
             pruneMarket(message);
             break;
@@ -34,19 +31,24 @@ const listChannels = () => {
 
     console.log(`Channels avaliable: \n`);
     chanList.map(c => {
-        console.log(`Id: ${c.id}`)
-        console.log(`Name: ${c.name} \n`)
+        console.log(`${c.id}`)
+        console.log(`  Name: ${c.name} \n`)
     });
 }
 
+const checkListings = (message) => {
+    const user = message.author;
+    const channel = message.channel;
+}
+
 const pruneMarket = (message) => {
-    const pruneDate = moment().subtract(config.pruneDays, 'days');
+    const pruneDate = moment().subtract(config.pruneAmount, 'days');
     let msgCollection = [];
 
     message.channel.fetchMessages()
         .then(messages => {
             let msgArr = messages.array();
-            
+
             msgCollection = msgArr.filter(msg => {
                 const timeStamp = msg.editedTimestamp === null ? moment(msg.createdAt) : moment(msg.editedTimestamp)
                 if (pruneDate > timeStamp) {
@@ -56,28 +58,27 @@ const pruneMarket = (message) => {
         })
         .then(e => message.channel.bulkDelete(msgCollection, true))
         .catch(console.error);
-        message.delete();
+    message.delete();
 }
 
-const closeBot = (message) => {
-    logCommand(message)
+const shutdown = (message) => {
+    
     message.channel.send('Beep boop, shutting down....')
-    .then(msg => client.destroy())
+        .then(msg => client.destroy())
 }
 
-const logCommand = (message) => {
+const logInput = (message) => {
     console.log(`Command "${message.content}" sent by ${message.author.username}:#${message.author.id} @ ${new Date()} in channel ${message.channel}`);
 }
 
-const getUserRole = (message) => {
+const authUser = (message) => {
     let allowed = false;
 
-    logCommand(message)
     for (let role in config.roles) {
         message.member.roles.find('name', config.roles[role])
-            allowed = true;
+        allowed = true;
     }
-    return allowed
+    return allowed;
 }
 
 // Log our bot in
