946f5882137a9dea7e87d36216fa48b8b2b9b614
Adam  Phillips <aphillips@squiz.net>
2018-09-07 09:05:46 +1000

    Bot now messages user when ad is removed

--- app.js
+++ app.js
@@ -6,33 +6,33 @@ const config = require("./config.json");
 
 client.on('ready', () => {
     console.log(`${moment()} : Snagbot started\n`);
+    listChannels();
 });
 
 client.on('message', message => {
-    // Ignore if bot or no auth
-    if(message.author.bot) return;
-    checkListings(message);
-    // Ignore if bot or no auth
-    if(!authUser(message)) return;
-    // Ensure bot only reacts in a valid channel
-    if (! config.channels.find(c => {
-            return c == message.channel.id
-        })) {
-            return false;
-    }
-    logInput(message);
+    // Ignore if bot 
+    if (message.author.bot) return;
 
-    switch (message.content) {
-        // Force shutdown bot
-        case `${config.prefix}shutdown`:
-            shutdown(message);
-            break;
-            // Cleanup the ad channel
-        case `${config.prefix}prune`:
-            pruneMarket(message);
-            checkListings(message);
-            break;
+    // Ensure bot only reacts in a valid channel
+    if (config.channels.find(c => c == message.channel.id)) {
+        checkListings(message);
     }
+
+    // Respond to allowed roles only
+    if (authUser(message)) {
+        logInput(message);
+
+        switch (message.content) {
+            // Force shutdown bot
+            case `${config.prefix}shutdown`:
+                shutdown(message);
+                break;
+                // Cleanup the ad channel
+            case `${config.prefix}prune`:
+                pruneMarket(message);
+                break;
+        }
+    };
 });
 
 const listChannels = () => {
@@ -45,17 +45,28 @@ const listChannels = () => {
 }
 
 const updateMarketRules = (channel) => {
+    const embed = new Discord.RichEmbed()
+        .setTitle("***Market rules***")
+        .setColor(0x00AE86)
+        .setDescription(`- One ad per user\n    - Posting new ads will remove your previous ads\n    - Provide photo of item if possible\n    - Please remove your post once sale is made\n - Ads older than two weeks will be removed automatically\n\n Any bugs with the bot, PM <@${config.authorId}>`);
+
     channel.fetchMessages({
-        limit: 100
-    })
-    .then(messages => {
-        const filtered = messages.filter(msg => msg.author.id === config.botId);
-        channel.bulkDelete(filtered, true)
-    })
-    .catch(console.error);
-    
-    channel.send(`**Market Rules**\n    - One ad per user\n    - Posting new ads will remove your previous ads\n    - Provide photo of item\n    - Please remove your post once sale is made\n\n Any bugs with the bot, PM <@${config.authorId}>`)
+            limit: 100
+        })
+        .then(messages => {
+            const filtered = messages.filter(msg => msg.author.id === config.botId);
+            channel.bulkDelete(filtered, true)
+        })
+        .catch(console.error);
+
+    channel.send({embed});
 };
+
+const notifyUser = (user, channel, message) => {
+    user.send(`An old ad from ${channel} was removed since you posted a new one. \nContents of the old ad are below \`\`\` ${message} \`\`\`If you think this is a mistake, PM the bot author: <@${config.authorId}>`)
+            .then(message => console.log(`Sent message: ${message.content}`))
+            .catch(console.error);
+}
 
 const checkListings = (message) => {
     const user = message.author;
@@ -71,12 +82,15 @@ const checkListings = (message) => {
             .then(messages => {
                 // Filter to only get the commenting users messages
                 const filtered = messages.filter(msg => msg.author.id === user.id && msg.id !== message.id);
+                const lastAd = filtered.last()
                 console.log(`${moment()} : User ${user.username}:${user.id} already has ${filtered.size} posts`)
                 console.log(`${moment()} : Deleting ${filtered.size} old posts`)
+                notifyUser(user, channel, lastAd.content)
                 message.channel.bulkDelete(filtered, false)
             })
             .catch(console.error);
-            updateMarketRules(channel)
+
+        updateMarketRules(channel)
     }
 }
 
@@ -102,7 +116,6 @@ const pruneMarket = (message) => {
 }
 
 const shutdown = (message) => {
-
     message.channel.send('Beep boop, shutting down....')
         .then(msg => client.destroy())
 }
