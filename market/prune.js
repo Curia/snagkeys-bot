75043f5b4455f81dc3b8df58cb49c28258de7b05
Adam  Phillips <aphillips@squiz.net>
2019-04-16 16:05:56 +1000

    Tested prune functionality and reformatted

--- market/prune.js
+++ market/prune.js
@@ -4,13 +4,12 @@ const moment = require("moment");
 const config = require("../config.json");
 
 const pruneMarket = (msg) => {
-    console.log(`Running prune of ${msg.channel}`);
 
     async function clear() {
-        const fetched = await msg.channel.fetchMessages({limit: 99});
-        const pruneDate = moment().clone().subtract(config.pruneDays, 'days').startOf('day');
+        const fetched = await msg.channel.fetchMessages({ limit: 99 });
         // Short timer for testing purposes
         //const pruneDate = moment().clone().subtract('30', 'seconds');
+        const pruneDate = moment().clone().subtract(config.pruneDays, 'days').startOf('day');
         const filtered = fetched.filter(message => moment(message.createdTimestamp).isBefore(pruneDate));
 
         console.log(`${filtered.size} messages to remove`)
